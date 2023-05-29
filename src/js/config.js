8f73836305dde26ef66d87784edd7f3427e3e6ca
Victorrr <122665852+Bictorrr@users.noreply.github.com>
2023-05-29 11:00:50 +0900

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'cec68f3b-ff8f-4a4f-901e-425adae8fc3d';
+export const MODAL_CLOSE_SEC = 2.5;
